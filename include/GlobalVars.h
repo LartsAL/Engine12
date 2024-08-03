a3743cdc09b6328b57d349024f65a3cf1eb1d532
Larts <alex1790007@mail.ru>
2024-08-03 19:01:14 +0500

    Changed pointer to ID

--- include/GlobalVars.h
+++ include/GlobalVars.h
@@ -4,12 +4,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using WindowID = GLuint;
+
 extern GLuint g_screenWidth;
 extern GLuint g_screenHeight;
 
 extern GLfloat g_deltaTime;
 
-extern std::shared_ptr<GLFWwindow> g_currentWindow;
+//extern std::shared_ptr<GLFWwindow> g_currentWindow;
+
+extern WindowID g_currentWindow;
 
 //extern void (*g_keyCallback)            (GLFWwindow*, int, int, int, int);
 //extern void (*g_cursorPositionCallback) (GLFWwindow*, double, double);
--- src/GlobalVars.cpp
+++ src/GlobalVars.cpp
@@ -5,4 +5,6 @@ GLuint g_screenHeight = 0;
 
 GLfloat g_deltaTime = 0.0f;
 
-std::shared_ptr<GLFWwindow> g_currentWindow = nullptr;
+//std::shared_ptr<GLFWwindow> g_currentWindow = nullptr;
+
+WindowID g_currentWindow = 0;
