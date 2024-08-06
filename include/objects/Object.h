052053546fc34fc7612c4650d88e11f70780e30d
grechishnikov.ma <grechishnikov.ma@dozen.mephi.ru>
2024-08-06 21:18:30 +0500

    object now has SceneID member

--- include/objects/Object.h
+++ include/objects/Object.h
@@ -8,9 +8,11 @@
 class Object {
 public:
     explicit Object(ObjectID ID);
+    explicit Object(ObjectID ID, SceneID sceneID);
     auto getID() const -> ObjectID;
     std::unordered_set<ComponentID> components; // TODO: a map in ComponentManager has the same name, rename everything
     //also unordered_set might not be memory-efficient here
 private:
     ObjectID ID{};
+    SceneID sceneID{};
 };
